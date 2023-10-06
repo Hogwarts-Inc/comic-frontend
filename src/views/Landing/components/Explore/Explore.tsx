69cb21ababa47f4e5daf807e52bd269b7f8e0888
Gonzalo Adrover <adrovergonzalo@gmail.com>
2023-10-06 14:44:43 -0300

    style fixes

--- src/views/Landing/components/Explore/Explore.tsx
+++ src/views/Landing/components/Explore/Explore.tsx
@@ -4,13 +4,18 @@ import { Button } from '@mui/material';
 
 import styles from './explore.module.css';
 
+const texts = {
+  description: 'Empeza a explorar otros capitulos y crear tus propias historias',
+  button: 'Lorem',
+};
+
 export const Explore = (): JSX.Element => (
   <div className={styles.box}>
     <div className={styles.placeholder} />
     <div className={styles.content}>
-      <p className={styles.description}>Empeza a explorar otros capitulos y crear tus propias historias</p>
+      <p className={styles.description}>{texts.description}</p>
       <Button size="large" variant="contained">
-        Lorem
+        {texts.button}
       </Button>
     </div>
   </div>
