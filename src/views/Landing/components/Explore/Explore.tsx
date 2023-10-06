278f1856ff6599a66be19d40dcb3bd9cd34a6fba
Gonzalo Adrover <adrovergonzalo@gmail.com>
2023-10-06 01:13:44 -0300

    style update

--- src/views/Landing/components/Explore/Explore.tsx
+++ src/views/Landing/components/Explore/Explore.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import { Button } from '@components/Button/Button';
+import { Button } from '@mui/material';
 
 import styles from './explore.module.css';
 
 export const Explore = (): JSX.Element => (
   <div className={styles.box}>
-    <div className={styles.exploreWrapper}>
-      <div className={styles.explore}>
-        <div className={styles.title}>
-          <p className={styles.description}>Empeza a explorar otros capitulos y crear tus propias historias</p>
-          <Button property1="default" />
-        </div>
-      </div>
+    <div className={styles.placeholder} />
+    <div className={styles.content}>
+      <p className={styles.description}>Empeza a explorar otros capitulos y crear tus propias historias</p>
+      <Button size="large" variant="contained">
+        Lorem
+      </Button>
     </div>
   </div>
 );
