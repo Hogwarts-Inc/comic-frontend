3d2534dbd8270547694d49733482dc388d280958
tomasabreu <42159630+tomasabreu@users.noreply.github.com>
2023-10-06 12:22:57 -0300

    Router setup

---
+++ pages/perfil.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Profile = () => <div>Perfil</div>;
+
+export default Profile;
---
+++ src/constants/routes.ts
@@ -0,0 +1,5 @@
+export const Route = {
+  editor: '/editor',
+  profile: '/perfil',
+  visualizer: '/visualizador',
+};
