2fa3face1e7e5f09026fa13024378f0b430d6c56
faulestrade <faustiles99@gmail.com>
2023-10-07 11:18:54 -0300

    add basic fetchs

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+
+//TO DO: Add types
+// type Canva = any;
+type canvaCreation = any;
+type canvaParam = any;
+type storiettesCreation = any;
+type storiettesParam = any;
+type chapterCreation = any;
+type chapterParam = any;
+type graphicResourcesCreation = any;
+type graphicResourcesParam = any;
+
 export const CONTENT_TYPE = {
   // Accept: '/',
   'Content-Type': 'application/json',
@@ -20,11 +32,35 @@ const api = axios.create({
 //   return req;
 // });
 
-//CANVAS
-const apis = {
-  getCanva: () => api.get("/canvas"),
-  getCanvaById: (id:number) => api.get(`/canvas/${id}`),
+//COMIC
+export const apisComic = {
+  getStoriettes: () => api.get("/storiettes"),
+  getStoriettesById: (id:number) => api.get(`/storiettes/${id}`),
+  postStoriettes: (data:storiettesCreation) => api.post("/storiettes",data),
+  patchStoriettes: (id:number,data:storiettesParam) => api.patch(`/storiettes/${id}`,data),
 };
 
+//CHAPTERS
+export const apisChapters = {
+  getCChapters: () => api.get("/chapters"),
+  getChaptersById: (id:number) => api.get(`/chapters/${id}`),
+  postChapters: (data:chapterCreation) => api.post("/chapters",data),
+  patchChapters: (id:number,data:chapterParam) => api.patch(`/chapters/${id}`,data),
+};
 
-export default apis;
+//CANVAS
+export const apisCanvas = {
+  getCanva: () => api.get("/canvas"),
+  getCanvaById: (id:number) => api.get(`/canvas/${id}`),
+  postCanva: (data:canvaCreation) => api.post("/canvas",data),
+  patchCanva: (id:number,data:canvaParam) => api.patch(`/canvas/${id}`,data),
+};
+
+//GRAPHIC RESOURCES
+export const apisGraphicResources = {
+  getGraphicResources: () => api.get("/graphic_resources"),
+  getGraphicResourcesById: (id:number) => api.get(`/graphic_resources/${id}`),
+  getGraphicResourcesByType: (type:string) => api.get(`/graphic_resources/resource_for_type?resource_type=${type}`),
+  postGraphicResources: (data:graphicResourcesCreation) => api.post("/graphic_resources",data),
+  patchGraphicResources: (id:number,data:graphicResourcesParam) => api.patch(`/graphic_resources/${id}`,data),
+};
