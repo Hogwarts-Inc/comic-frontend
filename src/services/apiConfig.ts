b60382b1f14537b8f48d301d838b5c50c48bdd2d
faulestrade <faustiles99@gmail.com>
2023-10-07 10:31:42 -0300

    add get canva fetch

---
+++ src/services/apiConfig.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+export const CONTENT_TYPE = {
+  // Accept: '/',
+  'Content-Type': 'application/json',
+};
+
+const api = axios.create({
+  baseURL: "http://164.152.19.10:3000/api/v1",
+  headers: { ...CONTENT_TYPE },
+});
+
+// api.interceptors.request.use(async req => {
+//   const { user } = getState().auth;
+//   const mcid = user?.MCID;
+//   req.headers = { ...req.headers };
+//   if (mcid && !req?.headers?.masterCustomerId) {
+//     req.headers.masterCustomerId = mcid;
+//   }
+//   return req;
+// });
+
+//CANVAS
+const apis = {
+  getCanva: () => api.get("/canvas"),
+  getCanvaById: (id:number) => api.get(`/canvas/${id}`),
+};
+
+
+export default apis;
