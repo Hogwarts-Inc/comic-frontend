a814f5cd004409bc9c31dbd151ab45c4a277c8d2
Faustina Lestrade <42162716+faulestrade@users.noreply.github.com>
2023-10-31 14:35:04 -0300

    add event view (#22)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { BoxContainer, BoxMui, TextButton, TypographyWhite } from './styles';
+import { BoxContainer, BoxMui, TextButton, TypographyDescription, TypographyWhite } from './styles';
 
 export const Footer = () => (
   <BoxMui>
     <BoxContainer>
       <TypographyWhite variant="h2">Startup 3</TypographyWhite>
-      <TypographyWhite>
+      <TypographyDescription>
         Startup Framework contains components and complex blocks which can easily be integrated into almost any design.
-      </TypographyWhite>
+      </TypographyDescription>
       <TextButton>Privacy Policy</TextButton>
       <TextButton>Terms</TextButton>
     </BoxContainer>
