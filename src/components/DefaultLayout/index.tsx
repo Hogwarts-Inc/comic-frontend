3ba236d6c16f45a042b11de9dd28467b9b59fa65
Gonzalo Adrover <adrovergonzalo@gmail.com>
2023-11-08 17:19:20 -0300

    page setup

---
+++ src/components/DefaultLayout/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react';
+
+import { Grid } from '@mui/material';
+
+import { Footer } from '@components/Footer';
+import { TopBar } from '@components/TopBar';
+
+import { LayoutContainer, StyledContainer, StyledTextureBackground } from './styles';
+
+interface DefaultLayoutProps {
+  children: ReactNode;
+}
+
+const DefaultLayout = ({ children }: DefaultLayoutProps) => (
+  <LayoutContainer>
+    <StyledTextureBackground />
+    <StyledContainer maxWidth={false} disableGutters>
+      <Grid container direction="column" spacing={2} style={{ flex: 1 }} justifyContent="center">
+        <Grid item>
+          <TopBar isAuthenticated={false} />
+        </Grid>
+        <Grid item xs={12} style={{ flex: 1 }}>
+          {children}
+        </Grid>
+        <Grid item container justifyContent="center">
+          <Footer />
+        </Grid>
+      </Grid>
+    </StyledContainer>
+  </LayoutContainer>
+);
+
+export default DefaultLayout;
---
+++ src/components/DefaultLayout/styles.ts
@@ -0,0 +1,36 @@
+import { Container, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import TextureBackground from 'src/utils/texture';
+
+export const StyledTextureBackground = styled(TextureBackground)({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  zIndex: -1,
+});
+
+export const LayoutContainer = styled('div')({
+  position: 'relative',
+  minHeight: '100vh',
+});
+
+export const StyledContainer = styled(Container)({
+  position: 'relative',
+  height: '100vh',
+  overflowY: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: 0,
+  zIndex: 1,
+});
+
+export const StyledGrid = styled(Grid)({
+  flex: 1,
+});
+
+export const StyledGridItem = styled(Grid)({
+  flex: 1,
+});
