f4cf0543704c367308431974c6c119f125a2aa4d
faulestrade <faustiles99@gmail.com>
2023-11-13 19:31:52 -0300

    add chapters preview list

--- src/components/ChaptersPreview/index.tsx
+++ src/components/ChaptersPreview/index.tsx
@@ -1,132 +1,80 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable no-unused-vars */
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
 
-interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
-  label: string;
-  minWidth?: number;
-  align?: 'right';
-  format?: (value: number) => string;
-}
+import { StoriettesParam, apisChapters } from 'src/services/apiConfig';
 
-const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
-  },
-];
+import { Title, TableRowMui, ReactionRow, TableMui } from './styles';
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
+function ChapterPreviewer() {
+  const [page, setPage] = useState(0);
+  const [dataChapter, setDataChapter] = useState<StoriettesParam[]>([]);
+  const [rowPage, setRowPage] = useState(5);
 
-function createData(name: string, code: string, population: number, size: number): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+  useEffect(() => {
+    apisChapters.getChapters().then(({ data }) => setDataChapter(data));
+  }, []);
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-function ChapterViewer() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePagina = (_event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(+event.target.value);
+  const handleChangeFilasPorPagina = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const options = { day: 'numeric', month: 'short', year: 'numeric' } as Intl.DateTimeFormatOptions;
+    return date.toLocaleDateString('en-US', options);
+  };
+
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell key={column.id} align={column.align} style={{ minWidth: column.minWidth }}>
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                {columns.map(column => {
-                  const value = row[column.id];
-                  return (
-                    <TableCell key={column.id} align={column.align}>
-                      {column.format && typeof value === 'number' ? column.format(value) : value}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+    <>
+      {/* To do: add chapter name */}
+      <Title variant="h2">Nombre historita</Title>
+      <TableMui>
+        <TableBody>
+          {dataChapter.slice(page * rowPage, page * rowPage + rowPage).map((row, index) => (
+            <TableRowMui key={row.id}>
+              {/* To do: this cell is going to be changed once the canvas has the img */}
+              <TableCell>
+                <img src="https://www.google.com/url?sE" alt="url" />
+              </TableCell>
+              <TableCell>{row.title}</TableCell>
+              <TableCell />
+              <TableCell>{formatDate(row.updated_at).toString()}</TableCell>
+              {/* To do: this cell is going to be changed once the chapter has reactions */}
+              <TableCell>
+                <ReactionRow>
+                  <FavoriteBorderOutlinedIcon />
+                  <div>30</div>
+                </ReactionRow>
+              </TableCell>
+              <TableCell>{`#${index + 1}`}</TableCell>
+            </TableRowMui>
+          ))}
+        </TableBody>
+      </TableMui>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={dataChapter.length}
+          rowsPerPage={rowPage}
+          page={page}
+          onPageChange={handleChangePagina}
+          onRowsPerPageChange={handleChangeFilasPorPagina}
+        />
+      </div>
+    </>
   );
 }
-export default ChapterViewer;
+export default ChapterPreviewer;
