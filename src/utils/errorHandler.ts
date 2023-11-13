bd2682d4881fdedba1adfa1c120536dfba5bbea4
faulestrade <faustiles99@gmail.com>
2023-11-12 21:39:59 -0300

    fix comments

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,8 +1,9 @@
+import i18next from 'i18next';
 import { toast } from 'react-toastify';
 
 export const errorHandler = (error: Error) => {
   console.error('Error:', error);
 
-  const errorMessage = 'Ocurrió un error inesperado. Por favor, inténtalo de nuevo.';
+  const errorMessage = i18next.t('errorHandler');
   toast.error(errorMessage, { autoClose: 5000 });
 };
