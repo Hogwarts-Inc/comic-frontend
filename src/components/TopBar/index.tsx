7dee7b989f08980fa2067159852b4ec5c9ee1da4
Gonzalo Adrover <38366485+gonzalo-adrover@users.noreply.github.com>
2023-12-11 11:27:39 -0300

    Update index.tsx

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -41,7 +41,7 @@ export const TopBar = dynamic(
           <AppBarMui>
             <ToolbarMui disableGutters>
               <ButtonBox onClick={onClickLogo}>
-                <StyledLogoIcon/>
+                <StyledLogoIcon />
               </ButtonBox>
               {isAuthenticated ? (
                 <Box>
