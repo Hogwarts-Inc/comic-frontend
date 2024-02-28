7a19acfe9d68326d0931ff3ddeb977f04b061937
Faustina Lestrade <42162716+faulestrade@users.noreply.github.com>
2024-02-28 19:28:16 -0300

    Mct 243 - Update footer (#66)
    * update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 import useOnScreen from 'src/hooks/useOnScreen';
+import { TermsAndConditions, apisTermsAndConditions } from 'src/services/api';
 
-import { BoxMui, TextButton, TypographyDescription, TypographyWhite } from './styles';
+import { BoxMui, TypographyWhite, TypographyWhiteLink } from './styles';
 
 export const Footer = ({ onFooterIsShowed }: { onFooterIsShowed?: (value: boolean) => void }) => {
   const ref = useRef(null);
+  const { t } = useTranslation();
   const isVisible = useOnScreen(ref);
   useEffect(() => {
     onFooterIsShowed?.(isVisible);
   }, [isVisible, onFooterIsShowed]);
+  const [termsAndConditions, setTermsAndConditions] = useState<TermsAndConditions | undefined>();
+
+  useEffect(() => {
+    apisTermsAndConditions.getTermsAndConditions().then(({ data }) => {
+      setTermsAndConditions(data);
+    });
+  }, []);
 
   return (
     <BoxMui ref={ref}>
-      <TypographyWhite variant="h2">Startup 3</TypographyWhite>
-      <TypographyDescription>
-        Startup Framework contains components and complex blocks which can easily be integrated into almost any design.
-      </TypographyDescription>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <TypographyWhite>
+          {t('footer.knowAboutArtesVisualesStart')}
+          <TypographyWhiteLink href={process.env.NEXT_PUBLIC_UCU_LINK} target="_blank">
+            {` ${t('footer.ArtesVisuales')} `}
+          </TypographyWhiteLink>
+          {t('footer.knowAboutArtesVisualesEnd')}
+        </TypographyWhite>
+      </div>
       <div style={{ display: 'flex' }}>
-        <TextButton>Privacy Policy</TextButton>
-        <TextButton>Terms</TextButton>
+        <TypographyWhite>
+          {t('footer.see')}
+          <TypographyWhiteLink href={termsAndConditions?.file_url} target="_blank" rel="noreferrer">
+            {t('footer.termsAndConditions')}
+          </TypographyWhiteLink>
+        </TypographyWhite>
       </div>
     </BoxMui>
   );
